41f54e69a54b0857d6c98d257c0a8f65a59a08b3
alaimos <stephane.alaimo@saftbatteries.com>
2022-07-11 15:41:53 +0200

    4 attempts

--- TSIClient/query/query_api.py
+++ TSIClient/query/query_api.py
@@ -631,13 +631,8 @@ class QueryApi():
                     logging.error("TSIClient: The query was unsuccessful, check the format of the function arguments.")
                     raise TSIQueryError(response["error"])
 
-            #if ((response["timestamps"] == []) and ('continuationToken' not in list(response.keys()))):
-            #    logging.critical("No data in search span for tag: {tag}".format(tag=colNames[i]))
-
-            if ('continuationToken' in list(response.keys())):
-                logging.critical("Continuation token with empty timestamp for tag: {tag}".format(tag=colNames[i]))
-
-            if ('continuationToken' in list(response.keys())):
+            if ((response["timestamps"] == []) and ('continuationToken' not in list(response.keys()))):
+                logging.critical("1st Continuation token with empty timestamp for tag: {tag}".format(tag=colNames[i]))
                 #logging.critical("*******Retrying with token*********")
                 headers = {
                     "x-ms-client-application-name": self._applicationName,
@@ -678,6 +673,131 @@ class QueryApi():
                         logging.error("TSIClient: The query was unsuccessful, check the format of the function arguments.")
                         raise TSIQueryError(response["error"])
             
+            if ((response["timestamps"] == []) and ('continuationToken' not in list(response.keys()))):
+                logging.critical("2nd Continuation token with empty timestamp for tag: {tag}".format(tag=colNames[i]))
+                #logging.critical("*******Retrying with token*********")
+                headers = {
+                    "x-ms-client-application-name": self._applicationName,
+                    "Authorization": authorizationToken,
+                    "Content-Type": "application/json",
+                    "cache-control": "no-cache",
+                    'x-ms-continuation': response['continuationToken'],
+                }
+                #logging.critical('headers Q2: {tt}'.format(tt=headers))
+                try:
+                    jsonResponse = requests.request(
+                        "POST",
+                        url,
+                        data=json.dumps(payload),
+                        headers=headers,
+                        params=querystring,
+                    )
+                    jsonResponse.raise_for_status()
+                except requests.exceptions.ConnectTimeout:
+                    logging.error("TSIClient: The request to the TSI api timed out.")
+                    raise
+                except requests.exceptions.HTTPError:
+                    logging.error("TSIClient: The request to the TSI api returned an unsuccessfull status code.")
+                    raise
+
+                response = json.loads(jsonResponse.text)
+
+                #logging.critical('response: {tt}'.format(tt=response))
+                
+                if "error" in response:
+                    if "innerError" in response["error"]:
+                        if response["error"]["innerError"]["code"] == "TimeSeriesQueryNotSupported":
+                            raise TSIStoreError(
+                                "TSIClient: Warm store not enabled in TSI environment: {id}. Set useWarmStore to False."
+                                    .format(id=self.environmentId),
+                            )
+                    else:
+                        logging.error("TSIClient: The query was unsuccessful, check the format of the function arguments.")
+                        raise TSIQueryError(response["error"])
+            
+            if ((response["timestamps"] == []) and ('continuationToken' not in list(response.keys()))):
+                logging.critical("3rd Continuation token with empty timestamp for tag: {tag}".format(tag=colNames[i]))
+                #logging.critical("*******Retrying with token*********")
+                headers = {
+                    "x-ms-client-application-name": self._applicationName,
+                    "Authorization": authorizationToken,
+                    "Content-Type": "application/json",
+                    "cache-control": "no-cache",
+                    'x-ms-continuation': response['continuationToken'],
+                }
+                #logging.critical('headers Q2: {tt}'.format(tt=headers))
+                try:
+                    jsonResponse = requests.request(
+                        "POST",
+                        url,
+                        data=json.dumps(payload),
+                        headers=headers,
+                        params=querystring,
+                    )
+                    jsonResponse.raise_for_status()
+                except requests.exceptions.ConnectTimeout:
+                    logging.error("TSIClient: The request to the TSI api timed out.")
+                    raise
+                except requests.exceptions.HTTPError:
+                    logging.error("TSIClient: The request to the TSI api returned an unsuccessfull status code.")
+                    raise
+
+                response = json.loads(jsonResponse.text)
+
+                #logging.critical('response: {tt}'.format(tt=response))
+                
+                if "error" in response:
+                    if "innerError" in response["error"]:
+                        if response["error"]["innerError"]["code"] == "TimeSeriesQueryNotSupported":
+                            raise TSIStoreError(
+                                "TSIClient: Warm store not enabled in TSI environment: {id}. Set useWarmStore to False."
+                                    .format(id=self.environmentId),
+                            )
+                    else:
+                        logging.error("TSIClient: The query was unsuccessful, check the format of the function arguments.")
+                        raise TSIQueryError(response["error"])
+            
+            if ((response["timestamps"] == []) and ('continuationToken' not in list(response.keys()))):
+                logging.critical("4th Continuation token with empty timestamp for tag: {tag}".format(tag=colNames[i]))
+                #logging.critical("*******Retrying with token*********")
+                headers = {
+                    "x-ms-client-application-name": self._applicationName,
+                    "Authorization": authorizationToken,
+                    "Content-Type": "application/json",
+                    "cache-control": "no-cache",
+                    'x-ms-continuation': response['continuationToken'],
+                }
+                #logging.critical('headers Q2: {tt}'.format(tt=headers))
+                try:
+                    jsonResponse = requests.request(
+                        "POST",
+                        url,
+                        data=json.dumps(payload),
+                        headers=headers,
+                        params=querystring,
+                    )
+                    jsonResponse.raise_for_status()
+                except requests.exceptions.ConnectTimeout:
+                    logging.error("TSIClient: The request to the TSI api timed out.")
+                    raise
+                except requests.exceptions.HTTPError:
+                    logging.error("TSIClient: The request to the TSI api returned an unsuccessfull status code.")
+                    raise
+
+                response = json.loads(jsonResponse.text)
+
+                #logging.critical('response: {tt}'.format(tt=response))
+                
+                if "error" in response:
+                    if "innerError" in response["error"]:
+                        if response["error"]["innerError"]["code"] == "TimeSeriesQueryNotSupported":
+                            raise TSIStoreError(
+                                "TSIClient: Warm store not enabled in TSI environment: {id}. Set useWarmStore to False."
+                                    .format(id=self.environmentId),
+                            )
+                    else:
+                        logging.error("TSIClient: The query was unsuccessful, check the format of the function arguments.")
+                        raise TSIQueryError(response["error"])
 
 
             if requestType == 'aggregateSeries':
